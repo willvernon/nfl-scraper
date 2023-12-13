08cb9e862ae1654b099487009ce1ee5a65c62f5d
willvernon <wmvernon@proton.me>
2023-12-13 12:03:42 -0500

    add: look through all weeks

--- nfl_scores_scraper/nfl_scores_scraper/spiders/__init__.py
+++ nfl_scores_scraper/nfl_scores_scraper/spiders/__init__.py
@@ -3,27 +3,57 @@ import scrapy
 
 class NFLScores(scrapy.Spider):
     name = "nfl_scores"
-    start_urls = ["https://www.pro-football-reference.com/"]
+    start_urls = [
+        "https://www.pro-football-reference.com/years/2023/week_1.htm",
+        "https://www.pro-football-reference.com/years/2023/week_2.htm",
+        "https://www.pro-football-reference.com/years/2023/week_3.htm",
+        "https://www.pro-football-reference.com/years/2023/week_4.htm",
+        "https://www.pro-football-reference.com/years/2023/week_5.htm",
+        "https://www.pro-football-reference.com/years/2023/week_6.htm",
+        "https://www.pro-football-reference.com/years/2023/week_7.htm",
+        "https://www.pro-football-reference.com/years/2023/week_8.htm",
+        "https://www.pro-football-reference.com/years/2023/week_9.htm",
+        "https://www.pro-football-reference.com/years/2023/week_10.htm",
+        "https://www.pro-football-reference.com/years/2023/week_11.htm",
+        "https://www.pro-football-reference.com/years/2023/week_12.htm",
+        "https://www.pro-football-reference.com/years/2023/week_13.htm",
+        "https://www.pro-football-reference.com/years/2023/week_14.htm",
+        "https://www.pro-football-reference.com/years/2023/week_15.htm",
+        "https://www.pro-football-reference.com/years/2023/week_16.htm",
+        "https://www.pro-football-reference.com/years/2023/week_17.htm",
+        "https://www.pro-football-reference.com/years/2023/week_18.htm",
+    ]
     custom_settings = {
-        'FEED_EXPORT_FIELDS': ["date", "winning_team", "winning_team_score", "losing_team", "losing_team_score"],
+        "FEED_EXPORT_FIELDS": [
+            "date",
+            "winning_team",
+            "winning_team_score",
+            "losing_team",
+            "losing_team_score",
+            "week",  # new field
+        ],
     }
+
     def parse(self, response):
         game = 1
-        while game <= 15:
+        week = (
+            response.url.split("/")[-1].split("_")[-1].split(".")[0]
+        )  # Extract week from URL
+        while game <= 16:
             date = response.xpath(
-                f'//*[@id="scores"]/div[2]/div[{game}]/table[1]/tbody/tr[1]/td/text()'
+                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[1]/td/text()'
             ).extract_first()
             winning_team = response.xpath(
-                f'//*[@id="scores"]/div[2]/div[{game}]/table[1]/tbody/tr[2]/td[1]/a/text()'
+                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[2]/td[1]/a/text()'
             ).extract_first()
             winning_team_score = response.xpath(
-                f'//*[@id="scores"]/div[2]/div[{game}]/table[1]/tbody/tr[2]/td[2]/text()'
+                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[2]/td[2]/text()'
             ).extract_first()
             losing_team = response.xpath(
-                f'//*[@id="scores"]/div[2]/div[{game}]/table[1]/tbody/tr[3]/td[1]/a/text()'
+                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[3]/td[1]/a/text()'
             ).extract_first()
             losing_team_score = response.xpath(
-                f'//*[@id="scores"]/div[2]/div[{game}]/table[1]/tbody/tr[3]/td[2]/text()'
+                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[3]/td[2]/text()'
             ).extract_first()
             yield {
                 "date": date,
@@ -31,6 +61,7 @@ class NFLScores(scrapy.Spider):
                 "winning_team_score": winning_team_score,
                 "losing_team": losing_team,
                 "losing_team_score": losing_team_score,
+                "week": week,
             }
             game += 1
 
