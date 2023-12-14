0c2fad49f737e176e9639565aebeb7ebea0d0f54
willvernon <wmvernon@proton.me>
2023-12-13 19:50:59 -0500

    fix: the winners vs loser

--- nfl_scores_scraper/nfl_scores_scraper/spiders/__init__.py
+++ nfl_scores_scraper/nfl_scores_scraper/spiders/__init__.py
@@ -4,8 +4,8 @@ import scrapy
 class NFLScores(scrapy.Spider):
     name = "nfl_scores"
     start_urls = [
-        #"https://www.pro-football-reference.com/years/2023/week_1.htm",
-         #"https://www.pro-football-reference.com/years/2023/week_2.htm",
+        #  "https://www.pro-football-reference.com/years/2023/week_1.htm",
+        # "https://www.pro-football-reference.com/years/2023/week_2.htm",
         #  "https://www.pro-football-reference.com/years/2023/week_3.htm",
         # "https://www.pro-football-reference.com/years/2023/week_4.htm",
         # "https://www.pro-football-reference.com/years/2023/week_5.htm",
@@ -17,10 +17,10 @@ class NFLScores(scrapy.Spider):
         # "https://www.pro-football-reference.com/years/2023/week_11.htm",
         # "https://www.pro-football-reference.com/years/2023/week_12.htm",
         # "https://www.pro-football-reference.com/years/2023/week_13.htm",
-        # "https://www.pro-football-reference.com/years/2023/week_14.htm",
+        "https://www.pro-football-reference.com/years/2023/week_14.htm",
         # "https://www.pro-football-reference.com/years/2023/week_15.htm",
         # "https://www.pro-football-reference.com/years/2023/week_16.htm",
-        "https://www.pro-football-reference.com/years/2023/week_17.htm",
+        # "https://www.pro-football-reference.com/years/2023/week_17.htm",
         # "https://www.pro-football-reference.com/years/2023/week_18.htm",
     ]
     custom_settings = {
@@ -40,20 +40,20 @@ class NFLScores(scrapy.Spider):
             response.url.split("/")[-1].split("_")[-1].split(".")[0]
         )  # Extract week from URL
         while game <= 16:
-            date = response.xpath(
-                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[1]/td/text()'
+            date = response.css(
+                f"#content > div.game_summaries > div:nth-child({game}) > table.teams > tbody > tr.date > td::text"
             ).extract_first()
-            winning_team = response.xpath(
-                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[2]/td[1]/a/text()'
+            winning_team = response.css(
+                f"#content > div.game_summaries > div:nth-child({game}) > table.teams > tbody > tr.winner > td:nth-child(1) > a::text"
             ).extract_first()
-            winning_team_score = response.xpath(
-                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[2]/td[2]/text()'
+            winning_team_score = response.css(
+                f"#content > div.game_summaries > div:nth-child({game}) > table.teams > tbody > tr.winner > td:nth-child(2)::text"
             ).extract_first()
-            losing_team = response.xpath(
-                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[3]/td[1]/a/text()'
+            losing_team = response.css(
+                f"#content > div.game_summaries > div:nth-child({game}) > table.teams > tbody > tr.loser > td:nth-child(1) > a::text"
             ).extract_first()
-            losing_team_score = response.xpath(
-                f'//*[@id="content"]/div[4]/div[{game}]/table[1]/tbody/tr[3]/td[2]/text()'
+            losing_team_score = response.css(
+                f"#content > div.game_summaries > div:nth-child({game}) > table.teams > tbody > tr.loser > td:nth-child(2)::text"
             ).extract_first()
             yield {
                 "date": date,
@@ -65,27 +65,40 @@ class NFLScores(scrapy.Spider):
             }
             game += 1
 
-    # def parse(self, response):
-    #     # Extract data from the response
-    #     # This is just an example and will need to be modified to suit the specific structure of the webpage you're scraping
-    #     for games in response.css("game_summaries"):
-    #         yield {
-    #             "data": games.xpath("//*[@id='scores']/div[2]/div[1]/table[1]/tbody/tr[2]/td[1]/a/text()").get(),
-    #             "date": games.xpath("/div[1]/table[1]/tbody/tr[1]/td/text()").get(),
-    #             "time": games.css("div.game_summary::text").get(),
-    #             "away_team": games.css("div.game_summary::text").get(),
-    #             "away_score": games.css("div.game_summary::text").get(),
-    #             "home_team": games.css("div.game_summary::text").get(),
-    #             "home_score": games.css("div.game_summary::text").get(),
-    #         }
 
-    #     for item in response.css("div.item"):
-    #         yield {
-    #             "title": item.css("h2.title::text").get(),
-    #             "description": item.css("p.description::text").get(),
-    #         }
+# ------------------------------------------------------------------------------------------
+# Data
 
-    #     # Follow links to next pages
-    #     # This is just an example and will need to be modified to suit the specific structure of the webpage you're scraping
-    #     for href in response.css("a.next-page::attr(href)"):
-    #         yield response.follow(href, self.parse)
+# Date
+# content > div.game_summaries > div:nth-child(1) > table.teams > tbody > tr.date > td
+# content > div.game_summaries > div:nth-child(6) > table.teams > tbody > tr.date > td
+
+# Winner
+# content > div.game_summaries > div:nth-child(1) > table.teams > tbody > tr.winner > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(2) > table.teams > tbody > tr.winner > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(3) > table.teams > tbody > tr.winner > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(4) > table.teams > tbody > tr.winner > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(5) > table.teams > tbody > tr.winner > td:nth-child(1) > a
+
+# Winner Score
+# content > div.game_summaries > div:nth-child(1) > table.teams > tbody > tr.winner > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(2) > table.teams > tbody > tr.winner > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(3) > table.teams > tbody > tr.winner > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(4) > table.teams > tbody > tr.winner > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(5) > table.teams > tbody > tr.winner > td:nth-child(2)
+
+# ------------------------------------------------------------------------------------------------------------
+
+# Loser
+# content > div.game_summaries > div:nth-child(1) > table.teams > tbody > tr.loser > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(2) > table.teams > tbody > tr.loser > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(3) > table.teams > tbody > tr.loser > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(4) > table.teams > tbody > tr.loser > td:nth-child(1) > a
+# content > div.game_summaries > div:nth-child(5) > table.teams > tbody > tr.loser > td:nth-child(1) > a
+
+# Loser Score
+# content > div.game_summaries > div:nth-child(1) > table.teams > tbody > tr.loser > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(2) > table.teams > tbody > tr.loser > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(3) > table.teams > tbody > tr.loser > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(4) > table.teams > tbody > tr.loser > td:nth-child(2)
+# content > div.game_summaries > div:nth-child(5) > table.teams > tbody > tr.loser > td:nth-child(2)
